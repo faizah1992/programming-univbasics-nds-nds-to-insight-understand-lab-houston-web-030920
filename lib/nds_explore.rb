e456a4ec9d4b221f7de7131fc540ff4182ad3bff
FAIZAH AHSAN <faizah1992@gmail.com>
2020-02-26 18:06:36 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,11 @@ def print_first_directors_movie_titles
 pretty_print_nds(directors_database)
 movies= directors_database[0][:movies]
 row_index=0 
+string=""
 while row_index<movies.count do
+  
+  
+  
   
   
 end
