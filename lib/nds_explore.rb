c88c7b67f1dabe2f756a595a1028e2f552212eb4
FAIZAH AHSAN <faizah1992@gmail.com>
2020-02-26 17:40:24 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -5,8 +5,9 @@ require 'directors_database'
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
   pp nds
-  directors_database()
+
 end
 
 def print_first_directors_movie_titles
+  return directors_database
 end
